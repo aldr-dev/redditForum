dcf5b6cbb9087fccee89b2a02717e603db2640b5
Gordus Alexander <aldrmiller@gmail.com>
2024-09-19 18:22:22 +0600

    реализовал вывод карточек на страницу, добавил стилей, добавил прелоудер.

--- frontend/src/features/posts/Post.tsx
+++ frontend/src/features/posts/Post.tsx
@@ -1,8 +1,47 @@
+import {useEffect} from 'react';
+import {useAppDispatch, useAppSelector} from '../../app/hooks';
+import {selectGetPostsLoading, selectPostsData} from './postsSlice';
+import {toast} from 'react-toastify';
+import {getPostsData} from './postsThunks';
+import {Box, CircularProgress, Typography} from '@mui/material';
+import InfoIcon from '@mui/icons-material/Info';
+import PostCard from './components/PostCard';
+
 const Post = () => {
+  const dispatch = useAppDispatch();
+  const postsData = useAppSelector(selectPostsData);
+  const loading = useAppSelector(selectGetPostsLoading);
+
+  useEffect(() => {
+    const fetchPostsData =  async () => {
+      try {
+        await dispatch(getPostsData()).unwrap();
+      } catch (error) {
+        toast.error('Произошла непредвиденная ошибка. Повторите попытку позже.');
+        console.error('Произошла непредвиденная ошибка. Повторите попытку позже. ' + error);
+      }
+    };
+
+    void fetchPostsData();
+  }, [dispatch]);
+
+
   return (
-    <div>
-      Post
-    </div>
+    <>
+      {loading ? (<CircularProgress sx={{color: '#ff4500'}}/>) : (
+        <>
+          {postsData.length === 0 ? (
+            <Typography sx={{display: 'flex', alignItems: 'center'}} variant="body2" color="#000"><InfoIcon />&nbsp;В данный момент список публикаций пуст, но мы уже работаем над их
+              добавлением. Спасибо за ваше терпение!</Typography>) : (
+            <Box display="flex" sx={{mb: 2, maxHeight: 'calc(100vh - 121px)', overflowX: 'auto', border: '1px solid #eee', borderRadius: '10px', padding: '15px'}} gap={1} flexWrap="wrap">
+              {postsData.map((post) => (
+                <PostCard key={post._id} post={post}/>
+              ))}
+            </Box>
+          )}
+        </>
+      )}
+    </>
   );
 };
 
--- frontend/src/features/posts/components/PostCard.tsx
+++ frontend/src/features/posts/components/PostCard.tsx
@@ -1,8 +1,63 @@
-const PostCard = () => {
+import React from 'react';
+import {PostsData} from '../../../types';
+import noImage from '../../../assets/images/no-image.png';
+import {Box, Card, CardContent, CardMedia, Grid, Typography} from '@mui/material';
+import dayjs from 'dayjs';
+import {Link} from 'react-router-dom';
+import {API_URL} from '../../../config';
+
+interface Props {
+  post: PostsData;
+}
+
+const PostCard: React.FC<Props> = ({post}) => {
+  const imageUrl = post.image ? `${API_URL}/${post.image}` : noImage;
+
   return (
-    <div>
-      PostCard
-    </div>
+    <Card
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        border: '1px solid #E0E0E0',
+        borderRadius: 2,
+        boxShadow: 'none',
+        padding: 1,
+        width: '100%',
+        marginBottom: 1,
+        backgroundColor: '#fff',
+        '&:hover': {
+          boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.1)',
+        },
+      }}>
+      <CardMedia
+        component="img"
+        sx={{
+          width: 120,
+          height: 120,
+          borderRadius: 1,
+          marginRight: 4,
+          objectFit: 'cover',
+        }}
+        image={imageUrl}
+        alt="post image"
+      />
+      <CardContent sx={{ padding: '8px 0', flex: 1 }}>
+        <Grid container direction="column" spacing={1}>
+          <Grid item>
+            <Typography variant="body1" color="textSecondary" fontWeight="bold">
+              Опубликовано: {dayjs(post.datetime).format('DD.MM.YYYY HH:mm')} / Автор: {post.user.username}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Box display="flex" justifyContent="space-between" alignItems="center">
+              <Typography component={Link} sx={{color: '#ff4500', '&:hover': {color: '#da3d02'}}} to={`/full-post/${post._id}`}>
+                {post.title}
+              </Typography>
+            </Box>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
   );
 };
 
